c107964381f70f6b1e3497fe08fd124109bd9d08
Hjshaffer <hjshaffer@eou.edu>
2022-07-20 14:57:51 -0700

    First push

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './images.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          big pp
+        </p>
+        <a
+          className="App-link"
+          href="https://youtube.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Youtube link
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
