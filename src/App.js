303f44535bde6f43314aa268435d55788c49fcba
Hjshaffer <hjshaffer@eou.edu>
2022-07-21 19:48:51 -0700

    text box

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          big pp
-        </p>
-        <a
-          className="App-link"
-          href="https://youtube.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Youtube link
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hayden Shaffer Senior capstone
+          </p>
+          <p>
+            Welcome!
+          </p>
+          <form>
+            <label>
+              Enter a comment:
+              <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          <a
+              className="App-link"
+              href="https://google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Documents
+          </a>
+        </header>
+
+      </div>
+
   );
 }
 
